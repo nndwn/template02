24965e1ea859c06811f11efcc3d204f4b98fa3f5
xenial <rifwan.toenjoy@gmail.com>
2021-09-24 16:28:56 +0700

    add javascript

---
+++ js/custom.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+        
+
+        var $item = $('#myCarousel_home.carousel .carousel-item'); 
+        var $wHeight = $(window).height();
+    
+        $item.height($wHeight); 
+        $item.addClass('full-screen');
+        $item.eq(0).addClass('active');
+
+        $('#myCarousel_home .carousel-item img').each(function() {
+          var $src = $(this).attr('src');
+          var $color = $(this).attr('data-color');
+          $(this).parent().css({
+            'background-image' : 'url(' + $src + ')',
+            'background-color' : $color
+          });
+          $(this).remove();
+        });
+
+        $(window).on('resize', function (){
+          $wHeight = $(window).height();
+          $item.height($wHeight);
+        });
+
+        $('#myCarousel_home.carousel').carousel({
+          // interval: 5500,
+          pause: "false"
+        });
+
+});
